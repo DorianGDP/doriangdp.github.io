090787507f042d2064b71c14350e48874adf55f1
DorianGDP <dorian.marty@gestiondepatrimoine.com>
2025-01-02 12:03:44 +0100

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -39,7 +39,7 @@ const ChatComponent = () => {
     }]);
 
     try {
-      const response = await fetch('http://localhost:5000/api/chat', {
+      const response = await fetch('https://chatbot-gdp.onrender.com/api/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
