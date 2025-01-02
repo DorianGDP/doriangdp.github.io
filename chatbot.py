ab614a5bbd11d512ce9a7cf57d6653305223e729
DorianGDP <dorian.marty@gestiondepatrimoine.com>
2025-01-02 16:22:31 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -281,7 +281,7 @@ class ChatBot:
             
             return response
 
-    async def build_acknowledgment(self, extracted_info: dict) -> str:
+    def build_acknowledgment(self, extracted_info: dict) -> str:
         """Construit un accusé de réception naturel des informations reçues"""
         acknowledgments = []
         
